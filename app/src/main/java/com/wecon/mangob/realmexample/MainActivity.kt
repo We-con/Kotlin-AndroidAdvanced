6367c6d48836728b25966602bf18c9b0b67437a6
octave08 <octave08b@gmail.com>
2017-09-03 00:24:57 +0900

    Add fab

--- app/src/main/java/com/wecon/mangob/realmexample/MainActivity.kt
+++ app/src/main/java/com/wecon/mangob/realmexample/MainActivity.kt
@@ -2,7 +2,10 @@ package com.wecon.mangob.realmexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
 import com.wecon.mangob.realmexample.model.Person
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,11 +14,14 @@ import kotlin.properties.Delegates
 class MainActivity : AppCompatActivity() {
 
     private var realm: Realm by Delegates.notNull()
+    private var isFabOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         container.removeAllViews()
+
+        setFabActions()
 
         realm = Realm.getDefaultInstance()
         realm.executeTransaction {
@@ -35,6 +41,64 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         realm.close()
     }
+
+    private fun setFabActions() {
+        fab.setOnClickListener {
+            animateFab()
+        }
+        fab_action_c.setOnClickListener(fabListener("action create"))
+        fab_action_r.setOnClickListener(fabListener("action read"))
+        fab_action_u.setOnClickListener(fabListener("action update"))
+        fab_action_d.setOnClickListener(fabListener("action delete"))
+    }
+
+    private fun fabListener(msg: String) = View.OnClickListener {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        animateFab()
+    }
+
+    private fun animateFab() {
+        when(isFabOpen) {
+            true -> closeFab()
+            false -> openFab()
+        }
+        isFabOpen = !isFabOpen
+    }
+
+    private fun openFab() {
+        AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_forward)?.let {
+            fab.startAnimation(it)
+        }
+
+        AnimationUtils.loadAnimation(applicationContext, R.anim.fab_open)?.let {
+            fab_action_c.startAnimation(it)
+            fab_action_r.startAnimation(it)
+            fab_action_u.startAnimation(it)
+            fab_action_d.startAnimation(it)
+        }
+        fab_action_c.isClickable = true
+        fab_action_r.isClickable = true
+        fab_action_u.isClickable = true
+        fab_action_d.isClickable = true
+    }
+
+    private fun closeFab() {
+        AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_backword)?.let {
+            fab.startAnimation(it)
+        }
+
+        AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)?.let {
+            fab_action_c.startAnimation(it)
+            fab_action_r.startAnimation(it)
+            fab_action_u.startAnimation(it)
+            fab_action_d.startAnimation(it)
+        }
+        fab_action_c.isClickable = false
+        fab_action_r.isClickable = false
+        fab_action_u.isClickable = false
+        fab_action_d.isClickable = false
+    }
+
 
     private fun showStatus(txt: String) {
         container.addView(TextView(this).apply { text = txt })
