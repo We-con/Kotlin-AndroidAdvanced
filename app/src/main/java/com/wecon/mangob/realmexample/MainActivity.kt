276da3c3be98052bd45d3ae4e87f25391e30be19
octave08 <octave08b@gmail.com>
2017-09-03 19:01:05 +0900

    Add ComplexQuery

--- app/src/main/java/com/wecon/mangob/realmexample/MainActivity.kt
+++ app/src/main/java/com/wecon/mangob/realmexample/MainActivity.kt
@@ -6,8 +6,11 @@ import android.support.design.widget.FloatingActionButton
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.TextView
+import com.wecon.mangob.realmexample.model.Cat
+import com.wecon.mangob.realmexample.model.Dog
 import com.wecon.mangob.realmexample.model.Person
 import io.realm.Realm
+import io.realm.Sort
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.properties.Delegates
@@ -30,6 +33,9 @@ class MainActivity : AppCompatActivity() {
             realm.deleteAll()
         }
 
+//        showStatus(complexReadWrite())
+//        readDog()
+//        readCat()
         showStatus("Perform basic Create/Read/Update/Delete (CRUD) operations...")
     }
 
@@ -136,5 +142,83 @@ class MainActivity : AppCompatActivity() {
             } ?: showStatus("Can't delete Old Person")
         }
     }
+
+    private fun complexReadWrite(): String {
+        var status = "\nPerforming complex Read/Write operation..."
+
+        // Open the default realm. All threads must use its own reference to the realm.
+        // Those can not be transferred across threads.
+        val realm = Realm.getDefaultInstance()
+        try {
+            // Add ten persons in one transaction
+            realm.executeTransaction {
+                val fido = realm.createObject(Dog::class.java)
+                fido.name = "fido"
+                for (i in 1..9) {
+                    val person = realm.createObject(Person::class.java, UUID.randomUUID().toString())
+                    person.name = "Person no. $i"
+                    person.age = i
+                    person.dog = fido
+
+                    // The field tempReference is annotated with @Ignore.
+                    // This means setTempReference sets the Person tempReference
+                    // field directly. The tempReference is NOT saved as part of
+                    // the RealmObject:
+                    person.tempReference = 42
+
+                    for (j in 0..i - 1) {
+                        val cat = realm.createObject(Cat::class.java)
+                        cat.name = "Cat_$j"
+                        person.cats.add(cat)
+                    }
+                }
+            }
+
+            // Implicit read transactions allow you to access your objects
+            status += "\nNumber of persons: ${realm.where(Person::class.java).count()}"
+
+            // Iterate over all objects
+            for (person in realm.where(Person::class.java).findAll()) {
+                val dogName: String = person?.dog?.name ?: "None"
+
+                status += "\n${person.name}: ${person.age} : $dogName : ${person.cats.size}"
+
+                // The field tempReference is annotated with @Ignore
+                // Though we initially set its value to 42, it has
+                // not been saved as part of the Person RealmObject:
+                check(person.tempReference == 0)
+            }
+
+            // Sorting
+            val sortedPersons = realm.where(Person::class.java).findAllSorted("age", Sort.DESCENDING)
+            status += "\nSorting ${sortedPersons.last().name} == ${realm.where(Person::class.java).findAll().first().name}"
+
+        } finally {
+            realm.close()
+        }
+        return status
+    }
+
+    private fun readDog() {
+        showStatus(">>Read Dog table")
+        realm.where(Dog::class.java).findAll()?.let {
+            when(it.size) {
+                0 -> showStatus("There is no dog")
+                else -> for(dog in it) showStatus("${dog.name}")
+            }
+        } ?: showStatus("Can't read dog")
+    }
+
+    private fun readCat() {
+        showStatus(">>Read Cat table")
+        realm.where(Cat::class.java).findAll()?.let {
+            when(it.size) {
+                0 -> showStatus("There is no cat")
+                else -> for(cat in it) showStatus("${cat.name}")
+            }
+        } ?: showStatus("Can't read cat")
+    }
+
+
 
 }
